7af2b75c2b5d376cb638803f1b94732fdf3791c8
AmrMohamed <66434864+Amr-al@users.noreply.github.com>
2022-03-09 13:33:53 +0200

    Create binpow.cpp

---
+++ references/binpow.cpp
@@ -0,0 +1,22 @@
+ll binpow(ll a,ll b) {
+    a %= MOD;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res%MOD;
+}
+----------------------------------------
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
